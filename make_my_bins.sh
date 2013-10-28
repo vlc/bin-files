74d7cace5e380a7599a94752a00dd3cf88a09d59
Nick Partridge <nkpart@gmail.com>
2013-10-28 18:32:00 +1000

    .

--- make_my_bins.sh
+++ make_my_bins.sh
@@ -5,4 +5,12 @@ clean_link()
 }
 
 curl -s "https://raw.github.com/nkpart/selecta/master/selecta" -o selecta
+chmod +x selecta
 clean_link selecta produca
+
+curl -s "https://raw.github.com/ghc-ios/ghc-ios-scripts/master/clang-xcode5-wrapper.hs" -o clang-xcode5-wrapper.hs
+ghc --make clang-xcode5-wrapper.hs -hidir /tmp -odir /tmp
+
+curl -s "https://gist.github.com/nkpart/7002846/raw/de797d32fb313386dc2f4bb065e84ff631279a0a/bijecta" -o bijecta
+chmod +x bijecta
+
